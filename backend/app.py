040bc223e22fc2b91bc532e839b7b895f42cb226
Ved2101 <166489392+Ved2101@users.noreply.github.com>
2026-05-28 11:56:30 +0530

    Removed crashing dependencies

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,24 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Chitraksha Backend Running Successfully"
+        "message": "Backend is working successfully"
     })
 
-@app.route("/api")
-def api():
+
+@app.route("/health")
+def health():
     return jsonify({
-        "status": "success"
+        "status": "healthy"
     })
+
 
 if __name__ == "__main__":
     app.run()
