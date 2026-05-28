2c1660282e0cdad1e4ef5c5ceb663b34cd8111ae
Ved2101 <166489392+Ved2101@users.noreply.github.com>
2026-05-28 11:37:16 +0530

    Fixed Python import path

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
-import sys
 import os
+import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BACKEND_DIR = os.path.join(BASE_DIR, "backend")
+
+sys.path.insert(0, BACKEND_DIR)
 
 from app import app
